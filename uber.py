fbbeac23b9710fd2c8463048c342ec0ce3c3c2d1
gabichulas <gabylopezromero13@gmail.com>
2023-06-06 14:00:19 -0300

    Errores solucionados

--- uber.py
+++ uber.py
@@ -1,6 +1,5 @@
 from graph import *
 import pickle as pk
-import json
 
 class GraphNode:
     vertex = None
@@ -14,15 +13,28 @@ def strToList(string):
     string = string.replace("{","")
     string = string.replace("}","")
     string = string.split(",")
+    string[len(string)-1] = string[len(string)-1].replace("\n","")
     return string
 
-    
+def strToListofLists(string):
+    list = []
+    for i in range(len(string)):
+        if string[i] == "[":
+            j = i
+            for k in range(i,len(string)):
+                if string[k] != ']':
+                    j += 1
+                else:
+                    break
+            subString = string[i+1:j]
+            subString = subString.split(",")
+            subString[len(subString)-1] = subString[len(subString)-1].replace("\n","")
+            list.append(subString)
+    return list
+
 map = open('uber\mapExample.txt')
 E = map.readline()
 A = map.readline()
 
-#newA = eval(A)
-
-#graph = createGraphUber(E,A)
-
-print(strToList(E))
+E = strToList(E)
+A = strToListofLists(A)
